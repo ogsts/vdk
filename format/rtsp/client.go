0c923ccb993809dd86d95317d02ed9af46ede88b
Karlis Ogsts <karlis.ogsts@gmail.com>
2021-10-03 12:34:03 +0300

    debug

--- format/rtsp/client.go
+++ format/rtsp/client.go
@@ -28,8 +28,8 @@ import (
 
 var ErrCodecDataChange = fmt.Errorf("rtsp: codec data change, please call HandleCodecDataChange()")
 
-var DebugRtp = false
-var DebugRtsp = false
+var DebugRtp = true
+var DebugRtsp = true
 var SkipErrRtpBlock = false
 
 const (
